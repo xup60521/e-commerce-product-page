6b83abc06b5729543f3a532e3bceeabb3632a00d
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-06-25 15:41:19 +0800

    finish lightbox

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,7 +9,11 @@ import Image4 from "@/assets/images/image-product-4.jpg";
 import Plus from "@/assets/images/icon-plus.svg?react";
 import Minus from "@/assets/images/icon-minus.svg?react";
 import { IoCartOutline } from "react-icons/io5";
-import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
+import { Fragment, useState } from "react";
+
 const images = [
     {
         thumbnail: Thumb1,
@@ -63,10 +67,16 @@ function ProductDetail() {
             </p>
             <div className="flex flex-col gap-2 -mt-2">
                 <div className="flex gap-4 items-end">
-                    <span className="font-kumbh font-bold text-lg">$125.00</span>
-                    <span className="text-xs h-fit font-kumbh bg-black px-2 py-1 rounded-md text-white">50%</span>
+                    <span className="font-kumbh font-bold text-lg">
+                        $125.00
+                    </span>
+                    <span className="text-xs h-fit font-kumbh bg-black px-2 py-1 rounded-md text-white">
+                        50%
+                    </span>
                 </div>
-                <span className="font-kumbh text-xs line-through font-semibold">$250.00</span>
+                <span className="font-kumbh text-xs line-through font-semibold">
+                    $250.00
+                </span>
             </div>
             <div className="flex items-center gap-4 py-8">
                 <div className="flex items-center overflow-hidden flex-shrink-0 bg-e_light_grayish_blue rounded-lg">
@@ -96,36 +106,157 @@ function DisplayImages(props: {
     setSelectedImage: React.Dispatch<React.SetStateAction<SelectedImage>>;
 }) {
     const { selectedImage, setSelectedImage } = props;
+    const [isLightboxOpen, setIsLightboxOpen] = useState(false);
     return (
-        <div className="flex flex-col lg:w-[40%] gap-6 flex-shrink-0">
-            <img
-                src={images[selectedImage]?.lg_photo}
-                alt=""
-                className="rounded-2xl"
+        <Fragment>
+            <Lightbox
+                isLightboxOpen={isLightboxOpen}
+                setIsLightboxOpen={setIsLightboxOpen}
+                selectedImage={selectedImage}
+                setSelectedImage={setSelectedImage}
             />
-            <div className="grid grid-cols-4 gap-6">
-                {images.map((item, index) => (
-                    <button
-                        className={`rounded-xl overflow-hidden transition ring-2 ${
-                            selectedImage === index
-                                ? "ring-e_orange"
-                                : "ring-transparent"
-                        }`}
-                        onMouseDown={() =>
-                            setSelectedImage(index as unknown as SelectedImage)
-                        }
+            <div className="flex flex-col lg:w-[40%] gap-6 flex-shrink-0">
+                <button
+                    className="rounded-2xl overflow-hidden"
+                    onMouseDown={() => setIsLightboxOpen(!isLightboxOpen)}
+                >
+                    <img
+                        src={images[selectedImage].lg_photo}
+                        alt="preview image"
+                        className=""
+                    />
+                    <span className="sr-only">open lightbox</span>
+                </button>
+                <div className="grid grid-cols-4 gap-6">
+                    {images.map((item, index) => (
+                        <button
+                            key={`imagesdsdasd ${item.lg_photo}`}
+                            className={`rounded-xl overflow-hidden transition ring-2 bg-white ${
+                                selectedImage === index
+                                    ? "ring-e_orange"
+                                    : "ring-transparent"
+                            }`}
+                            onMouseDown={() =>
+                                setSelectedImage(
+                                    index as unknown as SelectedImage
+                                )
+                            }
+                        >
+                            <img
+                                src={item.thumbnail}
+                                alt={index + " thumbnail"}
+                                className={`transition ${
+                                    selectedImage === index && "opacity-50"
+                                }`}
+                            />
+                            <span className="sr-only">{"photo " + index}</span>
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+function Lightbox(props: {
+    isLightboxOpen: boolean;
+    setIsLightboxOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    selectedImage: SelectedImage;
+    setSelectedImage: React.Dispatch<React.SetStateAction<SelectedImage>>;
+}) {
+    const {
+        isLightboxOpen,
+        setIsLightboxOpen,
+        selectedImage,
+        setSelectedImage,
+    } = props;
+    function StopPropagation(e: React.MouseEvent) {
+        e.stopPropagation();
+    }
+    if (isLightboxOpen) {
+        return (
+            <div className="lg:flex hidden justify-center items-center flex-col fixed w-screen h-screen bg-black bg-opacity-75 top-0 left-0 z-50" onMouseDown={()=>setIsLightboxOpen(false)}>
+                <div className="max-h-full flex flex-col w-[min(32.5vw,40rem)]">
+                    <div className="w-full flex justify-end py-2">
+                        <button
+                            className="text-white text-3xl"
+                            onMouseDown={(e) => {
+                                setIsLightboxOpen(false);
+                                e.stopPropagation();
+                            }}
+                        >
+                            <IoClose className="" fontWeight={"fill"} />
+                            <span className="sr-only">close lightbox</span>
+                        </button>
+                    </div>
+                    <div
+                        className="relative aspect-square"
+                        onMouseDown={StopPropagation}
                     >
                         <img
-                            src={item.thumbnail}
-                            alt={index + " thumbnail"}
-                            className={`transition ${
-                                selectedImage === index && "opacity-30"
-                            }`}
+                            src={images[selectedImage].lg_photo}
+                            alt="preview photo"
+                            className="rounded-2xl"
                         />
-                        <span className="sr-only">{"photo " + index}</span>
-                    </button>
-                ))}
+                        <button
+                            className="absolute left-0 top-[50%] -translate-y-[50%] -translate-x-[50%] bg-white size-12 rounded-full flex items-center justify-center text-lg transition hover:text-e_orange"
+                            onMouseDown={() =>
+                                setSelectedImage(
+                                    selectedImage === 0
+                                        ? 3
+                                        : ((selectedImage - 1) as SelectedImage)
+                                )
+                            }
+                        >
+                            <FaChevronLeft />
+                            <span className="sr-only">preview image</span>
+                        </button>
+                        <button
+                            className="absolute right-0 top-[50%] -translate-y-[50%] translate-x-[50%] bg-white size-12 rounded-full flex items-center justify-center text-lg transition hover:text-e_orange"
+                            onMouseDown={() =>
+                                setSelectedImage(
+                                    selectedImage === 3
+                                        ? 0
+                                        : ((selectedImage + 1) as SelectedImage)
+                                )
+                            }
+                        >
+                            <FaChevronRight />
+                            <span className="sr-only">next image</span>
+                        </button>
+                    </div>
+                    <div className="w-full flex flex-row gap-6 flex-shrink-0 min-w-0 justify-center py-8">
+                        {images.map((item, index) => {
+                            return (
+                                <button
+                                    key={`lightbox select images ${item.thumbnail}`}
+                                    className={`w-[17.5%] rounded-lg overflow-hidden ring-2 bg-white ${
+                                        selectedImage === index
+                                            ? "ring-e_orange"
+                                            : "ring-transparent"
+                                    }`}
+                                    onMouseDown={(e) => {
+                                        setSelectedImage(
+                                            index as unknown as SelectedImage
+                                        );
+                                        e.stopPropagation();
+                                    }}
+                                >
+                                    <img
+                                        src={item.thumbnail}
+                                        alt="image thumbnail"
+                                        className={`transition ${
+                                            selectedImage === index &&
+                                            "opacity-30"
+                                        }`}
+                                    />
+                                </button>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    return null;
 }
